67faf17fdfccd840af25ee3cc3f5faef80640bad
lisen.saka <lisen@dev.al>
2024-04-17 16:10:27 +0200

    hibernate exercise

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,83 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class App {
+    private static final SessionFactory sessionFactory = new Configuration()
+            .configure("hibernate.cfg.xml")
+            .buildSessionFactory();
+
+    public static void main(String[] args) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            long studentId = 2;
+
+//           *** method for saving new Student with Hibernate
+//            saveStudent(session, "John", "Doe", 41);
+
+//           ***  method for reading Student by id  Hibernate
+            getStudentById(session, studentId);
+
+
+//           *** method for updating Student by id  Hibernate
+//            Student studentById = getStudentById(session, studentId);
+//            updateStudentById(studentById);
+
+//           *** method for reading Student by id  Hibernate
+//            readAllValuesInDb(session);
+
+//           *** method for deleting Student by id  Hibernate
+//            Student studentById = getStudentById(session, 1);
+//            deleteStudent(session, studentById);
+
+            transaction.commit();
+        }
+
+    }
+
+    private static void deleteStudent(Session session, Student student) {
+        if (student == null) {
+            System.out.println("Student was not found!");
+        }
+        session.delete(student);
+        System.out.println("Student was successfully deleted!");
+    }
+
+    private static void readAllValuesInDb(Session session) {
+        List<Student> studentList = session.createQuery("from Student").list();
+        studentList.forEach(System.out::println);
+    }
+
+    private static void updateStudentById(Student studentById) {
+        studentById.setAge(20);
+        studentById.setFirstName("Sda_firstName");
+        studentById.setLastName("Sda_lastName");
+    }
+
+    private static Student getStudentById(Session session, long studentId) {
+//        Student studentById = session.get(Student.class, studentId);
+        Student studentById = (Student) session.createQuery("from Student where id = :studentId").setParameter("studentId", studentId).getSingleResult();
+
+        if (studentById != null) {
+            System.out.println("Student by id : " + studentById);
+        } else {
+            System.out.println("Student not found with id provided: " + studentId);
+        }
+        return studentById;
+    }
+
+    private static void saveStudent(Session session, String firstName, String lastName, int age) {
+        Student student = new Student(firstName, lastName, age);
+        session.save(student);
+    }
+
+
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Student() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
